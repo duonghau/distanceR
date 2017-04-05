6871f4523f31959dc7e92a2ad39bdedaaf090065
duonghau <duonghau.emarketing@gmail.com>
2017-04-05 16:11:34 +0700

    add vignette

---
+++ R/phylo.R
@@ -0,0 +1,15 @@
+library(ape)
+
+dist_phylo<-function(x, y){
+
+}
+
+dist_phylos <-function(x,y){
+  cluster_x = dist.nodes(x)
+  cluster_y = dist.nodes(y)
+
+}
+
+read_tree<-function(file_name){
+return(read.tree(file_name))
+}
